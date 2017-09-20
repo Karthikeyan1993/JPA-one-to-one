8a55afc7797eebcd613bcb2a65e408ba07f38152
Karthikeyan K <Karthikeyan.K@KGFSL.COM>
2017-09-20 15:47:46 +0530

    finalll committt

--- src/main/java/com/sample/OtmApplication.java
+++ src/main/java/com/sample/OtmApplication.java
@@ -1,21 +1,14 @@
 package com.sample;
 
-import org.h2.server.web.WebServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 import com.sample.entity.Registration;
 import com.sample.repository.RegistrationRepository;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class OtmApplication {
@@ -35,19 +28,6 @@ public class OtmApplication {
 			});
 
 		};
-	}
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.regex("/api.*")).build();
-	}
-
-	@Bean
-	public ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
-		registration.addUrlMappings("/console/*");
-		return registration;
 	}
 
 }
